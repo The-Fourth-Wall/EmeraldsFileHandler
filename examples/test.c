dcd532ee317e1593aaa1d1ca963b808674007188
Oblivious <me262thanos@gmail.com>
2021-03-17 21:50:28 +0200

    Added example code for the read handler.

---
+++ examples/test.c
@@ -0,0 +1,23 @@
+#include "../export/ReadHandler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char *line1 = NULL;
+    char *line2 = NULL;
+    struct read_handler *h = read_handler_new();
+    
+    if(read_handler_open(h, "file.txt")) {
+        line1 = read_handler_read_line(h);
+        line2 = read_handler_read_line(h);
+    }
+
+    printf("%s\n", line1);
+    printf("%s\n", line2);
+
+    free(line1);
+    free(line2);
+
+    read_handler_close(h);
+}
--- src/ReadHandler.c
+++
@@ -1,6 +0,0 @@
-#include "ReadHandler.h"
-
-int main(void) {
-    printf("%s\n", get_value());
-    return 0;
-}
